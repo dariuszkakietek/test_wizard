e4e9d62ee990464a6db689974ae18fd6c1ea99f4
Darek <dariuszkakietek@gmail.com>
2022-01-18 21:51:55 +0100

    before changes

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+var slider = tns({
+    container: '.slider',
+    items: 3,
+    gutter: 50,
+    controlsPosition: 'bottom',
+    navPosition: 'bottom',
+    autoplay: true,
+    autoplayButtonOutput: false,
+    // controlsContainer: '.custom-controls',
+
+
+
+
+
+
+
+});
